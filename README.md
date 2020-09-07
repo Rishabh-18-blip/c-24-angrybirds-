a223a595befccbd77bbc0bbec9c8087a634b404a
Rishabh-18-blip <70805821+Rishabh-18-blip@users.noreply.github.com>
2020-09-07 14:57:09 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# c-24-angrybirds-
+# Box Class
---
+++ sketch.js
@@ -0,0 +1,62 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+
+var engine, world;
+var box1;
+var box2;
+var box3;
+var box4;
+var box5;
+var Ground;
+var pig1;
+var pig3;
+var log1;
+var log3;
+var log4;
+var log5;
+var bird;
+
+function setup(){
+    var canvas = createCanvas(1200,400);
+    engine = Engine.create();
+    world = engine.world;
+
+    box1 = new Box(700,320,70,70);
+    box2 = new Box(920,320,70,70);
+    box3=new Box(700,240,70,70);
+    box4=new Box(920,240,70,70);
+    box5=new Box(810,160,70,70);
+
+    Ground=new ground(600,height,1200,20);
+
+    pig1=new Pig(810,350);
+    pig3=new Pig(810,220);
+
+    log1=new Log(810,260,300,PI/2);
+    log3=new Log(810,180,300,PI/2);
+    log4=new Log(760,120,150,PI/7);
+    log5=new Log(870,120,150,-PI/7);
+
+    bird= new Bird(100,100);
+
+}
+
+function draw(){
+    background(0);
+    Engine.update(engine);
+    box1.display();
+    box2.display();
+    box3.display();
+    box4.display();
+    box5.display();
+    Ground.display();
+    pig1.display();
+    pig3.display();
+    log1.display();
+    log3.display();
+    log4.display();
+    log5.display();
+    bird.display();
+
+}
